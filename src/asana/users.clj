90bb4836f8d49b8aa55b1949e7301a790afb0377
Jason Lewis <jason@decomplecting.org>
2015-05-19 03:58:01 -0400

    Add some basic user fns

--- src/asana/users.clj
+++ src/asana/users.clj
@@ -1,4 +1,19 @@
 (ns asana.users
-  (:require org.httpkit.client :as http))
+  (:require [org.httpkit.client :as http]
+            [asana.core :refer [api-call]]))
 
 (def ^:dynamic end-point "users")
+
+(defn get-users
+  "Get info about all users in organization."
+  [& [options]]
+  (api-call :get "users" nil options))
+
+(defn user
+  "Get info about a user."
+  [user & [options]]
+  (api-call :get "users/%s" [user] options))
+
+(defn me
+  "Get info about the current authorized user."
+  (api-call :get "users/me" nil options))
